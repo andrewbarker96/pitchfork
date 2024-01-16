183e3a2c827816d392bbf1baa944b387974a5c91
Andrew Barker <andrewbarker96@gmail.com>
2024-01-16 11:55:47 -0600

    Changes to be committed: 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/about',
-          destination: '/',
-          permanent: true,
-        },
-      ]
-    },
+
   }
   
   module.exports = nextConfig
