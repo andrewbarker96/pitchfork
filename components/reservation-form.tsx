d02091081f484706fd703ddc010ccbbfe64175ba
Andrew Barker <andrewbarker96@gmail.com>
2024-01-16 11:19:00 -0600

    Changes to be committed: 	modified:   components/reservation-form.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/reservation-form.tsx
+++ components/reservation-form.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { FormEvent } from 'react'
 import { DateCalendar } from '@mui/x-date-pickers'
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 const ReservationForm = () => {
   return (
-    <div><DateCalendar /></div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <div><DateCalendar /></div>
+    </LocalizationProvider>
   )
 }
 
