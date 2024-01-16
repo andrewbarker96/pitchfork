fe6efa144a1c126e6b53c36e5a1af16dd25149a4
Andrew Barker <andrewbarker96@gmail.com>
2024-01-16 11:45:24 -0600

    Changes to be committed: 	modified:   components/reservation-form.tsx 	modified:   globals.css 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/reservations.tsx

--- components/reservation-form.tsx
+++ components/reservation-form.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { FormEvent } from 'react'
-import { DateCalendar } from '@mui/x-date-pickers'
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers';
+import { Button } from '@mui/material';
 
 const ReservationForm = () => {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <div><DateCalendar /></div>
-    </LocalizationProvider>
-  )
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <form className = 'space-between md:flex-col'>
+            <div className = 'form'>
+                <DatePicker label="Date of Reservation" />
+            </div>
+            <div className = 'form'>
+                <TimePicker label="Time of Reservation" />
+            </div>
+            <div className = 'form'>
+                <Button variant='contained'>Submit</Button>
+            </div>
+        </form>
+        </LocalizationProvider>
+    );
 }
 
 export default ReservationForm
--- pages/reservations.tsx
+++ pages/reservations.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import RootLayout from '@/layout'
 import ReservationForm from '@/components/reservation-form'
+import Link from 'next/link'
 
 const reservations = () => {
   return (
     <RootLayout>
       <div>
+        <h1>Make a Reservation!</h1>
         <ReservationForm />
+      </div>
+      <br></br>
+      <div>
+        <h1>No times available?</h1>
+        <h2>Call us at <Link href="tel:+15555555555" style = {{color: 'blue'}}>555-555-5555</Link> to make a reservation!</h2>
       </div>
     </RootLayout>
   )
