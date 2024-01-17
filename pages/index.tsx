d301176b950b2020b04c3697664983430450725e
Andrew Barker <andrewbarker96@gmail.com>
2024-01-17 15:27:22 -0600

    Changes to be committed: 	new file:   components/skeletons/content-skeleton.tsx 	modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import RootLayout from "@/app/layout"
 import React from "react"
+import { Slide } from "@mui/material"
+import Skeleton from "@mui/material"
+import ImageSkeleton from "@/components/skeletons/content-skeleton"
 
 export default function HomePage() {
   return (
     <RootLayout>
       <h1>Welcome to my website!</h1>
+      <ImageSkeleton />
     </RootLayout>
   )
 }
